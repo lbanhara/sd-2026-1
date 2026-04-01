596eb1aa6d560bc849832160142141eec467c3d2
Liana Mara Cunha Banhara <p017250@rede.sp>
2026-03-31 22:29:53 -0300

    aula 31/03 parte 2

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,6 @@
+/* const { log } = require("console")
+const { resolve } = require("path")
+
 //declarativa
 function saudacao(nome){
     return `Olá ${nome}`
@@ -26,4 +29,48 @@ console.log(executarOperacao (sub, 5,3))
 const multiplicar = (a,b) => a*b
 console.log (multiplicar(4,2))
 
+function numero (numero = 10){
+    return `O número ${numero} é um número par`
+}
+//console.log(numero())
+console.log(numero(26))
 
+//anonima
+//(function() {
+  //      let segredo = 123
+    //    console.log("autoexecutavel")
+        
+//}())
+
+//console.log(segredo)
+
+function processar (valor, callback){
+    console.log("autoexecutavel")
+    callback(valor)
+
+}
+
+processar(10, (num) => console.log(`resultado ${num*2}`))
+
+function dobrar(num){
+    console.log(`o dobro do numero ${num} é: `, num * 2)
+
+}
+processar(10, dobrar) */
+
+async function exemplo(){
+    console.log("antes");
+    await new Promise(resolve => setTimeout(resolve, 1000) )
+    console.log("depois");
+}
+
+exemplo()
+
+function pegaNumero(){
+    return Promise.resolve(10)
+}
+async function executar(){
+    let num = await pegaNumero()
+    console.log(num)
+}
+executar()
