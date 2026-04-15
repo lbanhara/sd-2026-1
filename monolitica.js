fe677eb4bd7f87153854bce5bc51e7ba282b2f25
Liana Mara Cunha Banhara <p017250@rede.sp>
2026-04-14 21:36:26 -0300

    aplicação monolitica

--- monolitica.js
+++ monolitica.js
@@ -29,14 +29,82 @@ let pedidos = [];
 
 app.post('/usuarios', (req, res) => {
     const usuario = req.body;
+    usuario.id = usuarios.length + 1; // Gerar um ID simples para o usuário
     usuarios.push(usuario);
-    res.send({ message: "Usuário criado com sucesso" , usuario});
+    
+    //res.send({ message: "Usuário criado com sucesso" , usuario});
+    res.status(201).json({ message: "Usuário criado com sucesso" , usuario}); // Retorna status 201 para indicar que o recurso foi criado
 });
+
+app.get('/usuarios', (req, res) => {
+
+    res.status(200).json(usuarios); // Retorna a lista de usuários em formato JSON
+});
+
+app.get('/usuarios/:id', (req, res) => {
+//id que tem na url, está na lista?
+    const id = parseInt(req.params.id)
+
+    const usuario = usuarios.find(u => u.id === id);
+    if (!usuario) {
+        return res.status(404).json({ erro: "Usuário não encontrado" });
+    }   
+    res.status(200).json(usuario); // Retorna o usuário encontrado em formato JSON
+});
+
+
+app.put("/usuarios/:id", (req, res) => {
+
+    const id = parseInt(req.params.id)
+
+
+    const index = usuarios.findIndex(u => u.id === id)
+
+    if (index === -1){
+        return res.status(404).json({erro: "Usuário não encontrado"})
+    }
+
+    usuarios[index] = {...req.body, id}
+
+    res.status(200).json({message: "Usuário atualizado completamente", usuario: usuarios[index]})
+
+})
+
+app.patch('/usuarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = usuarios.findIndex(u => u.id === id);
+    if (index === -1) {
+        return res.status(404).json({ erro: "Usuário não encontrado" });
+    }
+
+    usuarios[index] = { ...usuarios[index], ...req.body }; // Atualiza o usuário com os dados fornecidos
+    res.status(200).json({ message: "Usuário atualizado parcialmente com sucesso", usuario: usuarios[index] }); // Retorna o usuário atualizado em formato JSON
+});
+
+
+app.delete('/usuarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = usuarios.findIndex(u => u.id === id);
+    if (index === -1) {
+        return res.status(404).json({ erro: "Usuário não encontrado" });
+    }
+
+    const removido = usuarios.splice(index, 1); // Remove o usuário do array 1 elemento na posição do index
+    res.status(200).json({ message: "Usuário deletado com sucesso", usuario: removido[0] }); // Retorna o usuário removido em formato JSON
+});
+
+
+
 
 app.post('/pedidos', (req, res) => {
     const pedido = req.body;
+    pedido.id = pedidos.length + 1; // Gerar um ID simples para o pedido
+
     pedidos.push(pedido);
-    res.send({ message: "Pedido criado com sucesso" , pedido}); 
+    
+    //res.send({ message: "Pedido criado com sucesso" , pedido}); 
+    res.status(201).json({ message: "Pedido criado com sucesso" , pedido}); // Retorna status 201 para indicar que o recurso foi criado 
+
 });
 
 app.get('/dados', (req, res) => {
@@ -44,6 +112,13 @@ app.get('/dados', (req, res) => {
     res.send({ usuarios, pedidos});
 
 });
+
+app.get('/pedidos', (req, res) => {
+    // lógica para obter pedidos
+    res.status(200).json({ pedidos});
+
+});
+
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
